6f97852fdf56ebdfeba474b04960d29537481161
NTChat <116424515+NTChat@users.noreply.github.com>
2025-09-19 15:35:00 +0700

    Update productList.js

--- public/assets/js/productList.js
+++ public/assets/js/productList.js
@@ -1,4 +1,4 @@
-// src/components/ProductList/ProductList.js
+// public/assets/js/ProductList.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useSearchParams, useNavigate } from 'react-router-dom';
