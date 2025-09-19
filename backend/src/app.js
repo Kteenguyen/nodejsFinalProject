69f3b4cac7603d14fa8be9a703220cf420aefc1a
Ktee <nguyenkhoatai2003@gmail.com>
2025-09-19 17:50:42 +0700

    Tái cấu trúc thư mục theo express

---
+++ backend/src/app.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Views and static
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../../views'));
+app.use(express.static(path.join(__dirname, '../../public')));
+
+// Parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// DB
+const { connectDB } = require('../../config/dbConnection');
+connectDB();
+
+// Routes (reuse existing route definitions)
+const siteRoutes = require('./routes/route');
+app.use('/', siteRoutes);
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+module.exports = app;
+
+
