9012effb33bb2604ce67659568a82e550c77ba66
Ktee <nguyenkhoatai2003@gmail.com>
2025-09-25 20:52:31 +0700

    completely create react app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';  // <-- Tailwind chỉ import ở đây
+import './styles/index.css';  // <-- Tailwind chỉ import ở đây
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
