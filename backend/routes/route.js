407804c32575a21eb6911d558e9df8681e67a080
Ktee <nguyenkhoatai2003@gmail.com>
2025-09-29 19:02:04 +0700

    fix taiwind

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/userModel');
+
+// Import route modules
 const userRoutes = require('./userRoute');
-
-
 const productRoutes = require('./productRoute');
 const cartRoutes = require('./cartRoute');
 const orderRoutes = require('./orderRoute');
+
+//API user routes
+router.use('/api/users', userRoutes);
+
+// API cart routes
 router.use('/api/cart', cartRoutes);
+
+// API product routes
+router.use('/api/products', productRoutes);
+
+// API order routes
 router.use('/api/orders', orderRoutes);
 
-// Define routes
+// Define root routes
 router.get('/', (req, res) => {
     res.redirect('/home');
 });
--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.set('views', path.join(__dirname, '../views'));
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(cors()) // Enable CORS;
 
+
 // Parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
