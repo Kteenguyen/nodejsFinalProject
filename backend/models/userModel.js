2a22850ae8a0f623f7db11eeb3420355d4356d57
Ktee <nguyenkhoatai2003@gmail.com>
2025-10-09 22:37:36 +0700

    login. register

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,30 +8,30 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true, trim: true, lowercase: true },
     phoneNumber: { type: String },
-    address: { type: String },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
     googleId: { type: String },
     provider: { type: String, enum: ['local', 'google'], default: 'local' },
-    // shippingAddresses: {
-    //     type: [
-    //         new mongoose.Schema(
-    //             {
-    //                 addressId: { type: String, required: true },
-    //                 label: { type: String },
-    //                 recipientName: { type: String, required: true },
-    //                 phoneNumber: { type: String, required: true },
-    //                 street: { type: String, required: true },
-    //                 ward: { type: String },
-    //                 district: { type: String },
-    //                 city: { type: String, required: true },
-    //                 country: { type: String, default: 'Vietnam' },
-    //                 postalCode: { type: String },
-    //                 isDefault: { type: Boolean, default: false }
-    //             },
-    //             { _id: false }
-    //         )
-    //     ],
-    //     default: []
-    // }
+    shippingAddresses: {
+        type: [
+            new mongoose.Schema(
+                {
+                    addressId: { type: String, required: true },
+                    label: { type: String },
+                    recipientName: { type: String, required: true },
+                    phoneNumber: { type: String, required: true },
+                    street: { type: String, required: true },
+                    ward: { type: String },
+                    district: { type: String },
+                    city: { type: String, required: true },
+                    country: { type: String, default: 'Vietnam' },
+                    postalCode: { type: String },
+                    isDefault: { type: Boolean, default: false }
+                },
+                { _id: false }
+            )
+        ],
+        default: []
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
