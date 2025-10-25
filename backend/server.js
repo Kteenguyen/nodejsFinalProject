071d039fbfdceefc92b8319c552ce35581392697
Ktee <nguyenkhoatai2003@gmail.com>
2025-10-25 17:02:18 +0700

    UI users 25/10

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
 const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const express = require('express');
 const cors = require('cors');
 const siteRoutes = require('./routes/route');
-
-require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 const port = Number(process.env.PORT) || 3001;
 
 app.use(cors({
-    origin: "http://localhost:3001",  // Đúng domain frontend
-    credentials: true,                // Cho phép cookie / token
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    origin: "http://localhost:3000",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
 }));
+
 
 // Parsers
 app.use(express.json());
